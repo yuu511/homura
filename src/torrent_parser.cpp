1efb30b79599a63dadf26c9080d6d3102219b25b
syaro <pipimi@tfwno.gf>
2019-11-13 17:29:05 +0000

    begin work on torrent parser

--- src/torrent_parser.cpp
+++ src/torrent_parser.cpp
@@ -1,9 +1,16 @@
 #include <cstdlib>
+#include <cstdio>
+#include <memory>
+#include <boost/make_shared.hpp>
 #include <libtorrent/session.hpp>
 #include <libtorrent/add_torrent_params.hpp>
 #include <libtorrent/torrent_handle.hpp>
 #include <libtorrent/magnet_uri.hpp>
 #include <libtorrent/torrent_info.hpp>
+
+#include <libtorrent/extensions/ut_metadata.hpp>
+#include <libtorrent/extensions/ut_pex.hpp>
+#include <libtorrent/extensions/smart_ban.hpp>
 
 #include "torrent_parser.h"
 #include "errlib.h"
@@ -15,17 +22,23 @@ torrent_parser::torrent_parser()
 
 void torrent_parser::extract_magnet_information(std::string magnet) 
 {
-  fprintf (stderr,"magnet %s",magnet.c_str());
+  fprintf (stderr,"magnet %s ",magnet.c_str());
   lt::session s;
+  s.add_extension(&libtorrent::create_ut_metadata_plugin);
+  s.add_extension(&libtorrent::create_ut_pex_plugin);
+  s.add_extension(&libtorrent::create_smart_ban_plugin);
+  // s.start_lsd();
+  // s.start_upnp();
+  // s.start_natpmp();
+
   lt::add_torrent_params p;
   p.url = magnet;
   p.save_path = "/tmp/";
   p.flags |= p.flag_upload_mode;
   p.flags &= ~p.flag_auto_managed;
   lt::torrent_handle h = s.add_torrent(p);
-  // add the info hash
-  // auto info = lt::torrent_info(p.info_hash);
-  // while (!info.is_valid())
-  // {;}
-  // fprintf(stderr," %s\n",info.name().c_str());
+  while(!h.is_valid()){;}
+  p.ti = boost::make_shared<lt::torrent_info>(h.info_hash());
+  // auto info = lt::torrent_info(h.info_hash());
+  std::fprintf(stderr,"name %d\n",p.ti->num_pieces());
 }
