0d2cfb15e36b9282103f9996c26016d9de7f0026
eli <rrrolento@yahoo.com>
2020-12-01 20:43:01 -0800

    update automation_scripts

--- automation_scripts/download_example.py
+++ automation_scripts/download_example.py
@@ -11,10 +11,9 @@ import getopt,sys
 from pathlib import Path
 from clutch import Client
 
-def download(download_all):
-  client = Client(address="http://localhost:9091/transmission/rpc",username='transmission',password='transmission')
+def scrape_nyaasi(client):
+  csv_file = os.path.join (os.path.dirname(os.path.realpath(__file__)),'shows_to_download_kuro.csv')
   torrents_to_check = []
-  csv_file = os.path.join (os.path.dirname(os.path.realpath(__file__)),'shows_to_download.csv')
 
   with open (csv_file ,newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -46,28 +45,35 @@ def download(download_all):
         if torrent_info.arguments.torrent_added:
           print("Torrent %s added at %s" % (torrent_info.arguments.torrent_added.name,datetime.datetime.now()))
           torrents_to_check.append(torrent_info.arguments.torrent_added.id)
-      proc.wait()
-      torrent_magnet = output.decode("utf-8")
-      torrent_info = client.torrent.add({"filename":torrent_magnet, "download_dir":save_dest})      
-      if torrent_info.arguments.torrent_added:
-        print("Torrent %s added at %s" % (torrent_info.arguments.torrent_added.name,datetime.datetime.now()))
-        torrents_to_check.append(torrent_info.arguments.torrent_added.id)
+  return torrents_to_check
 
-    still_downloading = True
-    if torrents_to_check:
-      while still_downloading:
-        still_downloading = False
-        for id in torrents_to_check:
-          torrent_info = client.torrent.accessor(fields=['percent_done','name'],ids=id)  
-          if torrent_info.result and torrent_info.result=='success':
-            if ((torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['percent_done']) != 1.0):
-              still_downloading = True
-            else:
-              print ("Torrent %s completed at %s" % (torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['name'],datetime.datetime.now()))
-              continue
-        if still_downloading:
-          time.sleep(30)
-      requests.get('http://localhost:32400/library/sections/1/refresh?X-Plex-Token=__YOURTOKENHERE__')
+def wait_download_complete(client, torrents_to_check):
+  still_downloading = True
+  if torrents_to_check:
+    while still_downloading:
+      still_downloading = False
+      for id in torrents_to_check:
+        torrent_info = client.torrent.accessor(fields=['percent_done','name'],ids=id)  
+        if torrent_info.result and torrent_info.result=='success':
+          percent_done = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['percent_done']
+          name = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['name']
+          if (percent_done != 1.0):
+            still_downloading = True
+          else:
+            print ("Torrent %s completed at %s" % (name, datetime.datetime.now()))
+            continue
+      if still_downloading:
+        time.sleep(30)
+
+def plexhooks():
+  requests.get('http://localhost:32400/library/sections/1/refresh?X-Plex-Token=__YOURTOKENHERE__')
+
+def download(download_all):
+  client = Client(address="http://localhost:9091/transmission/rpc",username='transmission',password='transmission')
+  torrents_to_check = scrape_nyaasi(client)
+  if torrents_to_check:
+    wait_download_complete(client, torrents_to_check)
+    plexhooks()
 
 if __name__ == "__main__":
     print ("Script started at %s." % datetime.datetime.now()) 
