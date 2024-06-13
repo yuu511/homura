49fb0798c2ea975a245844d9b171258c66c27377
eli <rrrolento@yahoo.com>
2024-06-12 20:05:12 -0700

    Update script

--- automation_scripts/download_example.py
+++ automation_scripts/download_example.py
@@ -15,14 +15,32 @@ def scrape_nyaasi(client):
   csv_file = os.path.join (os.path.dirname(os.path.realpath(__file__)),'shows_to_download_kuro.csv')
   torrents_to_check = []
 
+
   with open (csv_file ,newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+    base_path = "/home/elijah/anime3/アニメ/"
+    copy_path = "/home/elijah/anime3/plex_アニメ/"
     for row in reader:
       search_term = row['Search_Term']
       save_dest = row['Save_Destination']
+      renamefmt = row['OPTIONAL_rename']
+
+      basedir = os.path.join(base_path,save_dest)
+      copydir = os.path.join(copy_path,save_dest)
+
+      if (not os.path.exists(basedir)):
+        print ("Made directory %s" % basedir)
+        os.mkdir(basedir)
+
+      if (not os.path.exists(copydir)):
+        print ("Made directory %s" % copydir)
+        os.mkdir(copydir)
+
+      tuple = {}
+      
       torrent_magnet = None
       output = None
-      proc = subprocess.Popen( ['homura', '--torrents_only', '--delay_end', 'search', search_term], stdout=subprocess.PIPE )
+      proc = subprocess.Popen( ['homura', '--refresh_cache', '--torrents_only', '--delay_end', 'search', search_term], stdout=subprocess.PIPE )
       if download_all:
         output = proc.communicate()
         if output and output[0]:
@@ -31,39 +49,95 @@ def scrape_nyaasi(client):
           if not magnets:
             continue
           for magnet in magnets:
-            torrent_info = client.torrent.add({"filename":magnet, "download_dir":save_dest})      
+            tuple = {}
+            torrent_info = client.torrent.add({"filename":magnet, "download_dir":str(basedir)})      
             if torrent_info.arguments.torrent_added:
               print("Torrent %s added at %s" % (torrent_info.arguments.torrent_added.name,datetime.datetime.now()))
-              torrents_to_check.append(torrent_info.arguments.torrent_added.id)
+              tuple["torrent_id"] = torrent_info.arguments.torrent_added.id
+
+              if (copydir != ""):
+                tuple["copy_dir"] = str(copydir)
+              else:
+                tuple["copy_dir"] = None
+
+              if (renamefmt != ""):
+                tuple["rename_fmt"] = renamefmt
+              else:
+                tuple["rename_fmt"] = None
+
+              torrents_to_check.append(tuple)
       else:
         output = subprocess.check_output((['head','-n1']),stdin=proc.stdout)
         proc.wait()
         torrent_magnet = output.decode("utf-8")
         if not torrent_magnet:
           continue
-        torrent_info = client.torrent.add({"filename":torrent_magnet, "download_dir":save_dest})      
+        torrent_info = client.torrent.add({"filename":torrent_magnet, "download_dir":str(basedir)})      
         if torrent_info.arguments.torrent_added:
           print("Torrent %s added at %s" % (torrent_info.arguments.torrent_added.name,datetime.datetime.now()))
-          torrents_to_check.append(torrent_info.arguments.torrent_added.id)
+
+          tuple["torrent_id"] = torrent_info.arguments.torrent_added.id
+
+          if (copydir != ""):
+            tuple["copy_dir"] = str(copydir)
+          else:
+           tuple["copy_dir"] = None
+
+          if (renamefmt != ""):
+            tuple["rename_fmt"] = renamefmt
+          else:
+            tuple["rename_fmt"] = None
+          torrents_to_check.append(tuple)
+
   return torrents_to_check
 
 def wait_download_complete(client, torrents_to_check):
-  still_downloading = True
   if torrents_to_check:
-    while still_downloading:
-      still_downloading = False
-      for id in torrents_to_check:
-        torrent_info = client.torrent.accessor(fields=['percent_done','name'],ids=id)  
-        if torrent_info.result and torrent_info.result=='success':
-          percent_done = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['percent_done']
-          name = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['name']
-          if (percent_done != 1.0):
-            still_downloading = True
-          else:
-            print ("Torrent %s completed at %s" % (name, datetime.datetime.now()))
-            continue
-      if still_downloading:
-        time.sleep(30)
+    i = 0
+    while torrents_to_check:  
+      tuple = torrents_to_check[i]
+      id = tuple["torrent_id"]
+      torrent_info = client.torrent.accessor(fields=['percent_done','name','files','download_dir'],ids=id)  
+      if torrent_info.result and torrent_info.result=='success':
+        percent_done = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['percent_done']
+        name = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['name']
+        download_dir = torrent_info.dict(exclude_none=True)["arguments"]["torrents"][0]['download_dir']
+        if (percent_done != 1.0):
+          i +=1
+          if (torrents_to_check and i == (len(torrents_to_check))):
+            i = 0
+            time.sleep(30)
+        else:
+          fullpath = os.path.join(download_dir,name)
+          print ("Torrent %s completed at %s" % (name, datetime.datetime.now()))
+          print (tuple["copy_dir"])
+          if (tuple["copy_dir"] != None):
+            proc = subprocess.Popen( ['cp', '-a', '-l','-v', fullpath , tuple["copy_dir"]], stdout=subprocess.PIPE )
+            output = proc.communicate()
+            if (proc.returncode == 0):
+              # file_format = 's/\[.*\][_ *](.*)[_\- ]+ *(\d+).*(\.\w+)/$1 s01e$2$3/'
+              if (tuple["rename_fmt"] != None):
+                fullpath = os.path.join(tuple["copy_dir"],name)
+                print ("Linking to %s" % fullpath)
+                print ("rename_fmt %s" % tuple["rename_fmt"])
+
+                # proc = subprocess.Popen( ['rename', '-n', tuple["rename_fmt"], fullpath], stdout=subprocess.PIPE )
+                # output = proc.communicate()
+                # print (output)
+                # proc = subprocess.Popen( ['rename', tuple["rename_fmt"], fullpath], stdout=subprocess.PIPE )
+                # output = proc.communicate()
+
+                # fk this, fuck python. this is how you get it to work....
+                os.chdir(tuple["copy_dir"])
+                cmdstring = "rename " + tuple["rename_fmt"] + " " + "*.mkv"
+                print ( "%s" % cmdstring)
+                os.system(cmdstring)
+                cmdstring = "rename " + tuple["rename_fmt"] + " " + "*.mp4"
+                print ( "%s" % cmdstring)
+                os.system(cmdstring)
+          del torrents_to_check[i] 
+          if (torrents_to_check and i >= len(torrents_to_check)):
+            i = 0
 
 def plexhooks():
   requests.get('http://localhost:32400/library/sections/1/refresh?X-Plex-Token=__YOURTOKENHERE__')
