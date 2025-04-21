9dcb0f9a6be9ff1f6074f19b5a52674a18a745af
eli <rrrolento@yahoo.com>
2025-04-21 02:30:43 -0700

    refactor --all code to simplify the case

--- automation_scripts/download_example.py
+++ automation_scripts/download_example.py
@@ -19,7 +19,6 @@ def scrape_nyaasi(client):
   csv_file = os.path.join (os.path.dirname(os.path.realpath(__file__)),'shows_to_download_kuro.csv')
   torrents_to_check = []
 
-
   with open (csv_file ,newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     base_path = "/home/elijah/anime3/アニメ/"
@@ -28,7 +27,7 @@ def scrape_nyaasi(client):
       search_term = row['Search_Term']
       save_dest = row['Save_Destination']
       renamefmt = row['OPTIONAL_rename']
-      whisper = row['OPTIONAL_whisper']
+      wsp = row['OPTIONAL_whisper']
 
       basedir = os.path.join(base_path,save_dest)
       copydir = os.path.join(copy_path,save_dest)
@@ -41,11 +40,9 @@ def scrape_nyaasi(client):
         print ("Made directory %s" % copydir)
         os.mkdir(copydir)
 
-      tuple = {}
-      
-      torrent_magnet = None
-      output = None
+      magnets = []
       proc = subprocess.Popen( ['homura', '--refresh_cache', '--torrents_only', '--delay_end', 'search', search_term], stdout=subprocess.PIPE )
+
       if download_all:
         output = proc.communicate()
         if output and output[0]:
@@ -53,58 +50,37 @@ def scrape_nyaasi(client):
           magnets = torrent_magnet.splitlines()
           if not magnets:
             continue
-          for magnet in magnets:
-            tuple = {}
-            torrent_info = client.torrent.add({"filename":magnet, "download_dir":str(basedir)})      
-            if torrent_info.arguments.torrent_added:
-              print("Torrent %s added at %s" % (torrent_info.arguments.torrent_added.name,datetime.datetime.now()))
-              tuple["torrent_id"] = torrent_info.arguments.torrent_added.id
-
-              if (copydir != ""):
-                tuple["copy_dir"] = str(copydir)
-              else:
-                tuple["copy_dir"] = None
-
-              if (renamefmt != ""):
-                tuple["rename_fmt"] = renamefmt
-              else:
-                tuple["rename_fmt"] = None
-
-              if (whisper == "TRUE"):
-                tuple["whisper"] = whisper
-              else:
-                tuple["whisper"] = None
-
-              torrents_to_check.append(tuple)
       else:
         output = subprocess.check_output((['head','-n1']),stdin=proc.stdout)
         proc.wait()
         torrent_magnet = output.decode("utf-8")
         if not torrent_magnet:
           continue
-        torrent_info = client.torrent.add({"filename":torrent_magnet, "download_dir":str(basedir)})      
+        magnets.append(torrent_magnet)
+        
+      for magnet in magnets:
+        tuple = {}
+        torrent_info = client.torrent.add({"filename":magnet, "download_dir":str(basedir)})      
         if torrent_info.arguments.torrent_added:
           print("Torrent %s added at %s" % (torrent_info.arguments.torrent_added.name,datetime.datetime.now()))
-
           tuple["torrent_id"] = torrent_info.arguments.torrent_added.id
 
           if (copydir != ""):
             tuple["copy_dir"] = str(copydir)
           else:
-           tuple["copy_dir"] = None
+            tuple["copy_dir"] = None
 
           if (renamefmt != ""):
             tuple["rename_fmt"] = renamefmt
           else:
             tuple["rename_fmt"] = None
 
-          if (whisper != ""):
-            tuple["whisper"] = whisper
+          if (wsp == "TRUE"):
+            tuple["wsp"] = wsp
           else:
-            tuple["whisper"] = None
+            tuple["wsp"] = None
 
           torrents_to_check.append(tuple)
-
   return torrents_to_check
 
 def wait_download_complete(client, torrents_to_check,model):
@@ -131,10 +107,9 @@ def wait_download_complete(client, torrents_to_check,model):
             proc = subprocess.Popen( ['cp', '-a', '-l','-v', fullpath , tuple["copy_dir"]], stdout=subprocess.PIPE )
             output = proc.communicate()
             if (proc.returncode == 0):
-              # file_format = 's/\[.*\][_ *](.*)[_\- ]+ *(\d+).*(\.\w+)/$1 s01e$2$3/'
               fullpath = os.path.join(tuple["copy_dir"],name)
 
-              if (tuple["whisper"] != None):
+              if (tuple["wsp"] != None):
                 print("Creating subs by whisper")
                 result = model.transcribe(fullpath, language="ja",condition_on_previous_text=False)
                 writer = get_writer("srt", tuple["copy_dir"])
@@ -143,14 +118,6 @@ def wait_download_complete(client, torrents_to_check,model):
               if (tuple["rename_fmt"] != None):
                 print ("Linking to %s" % fullpath)
                 print ("rename_fmt %s" % tuple["rename_fmt"])
-
-                # proc = subprocess.Popen( ['rename', '-n', tuple["rename_fmt"], fullpath], stdout=subprocess.PIPE )
-                # output = proc.communicate()
-                # print (output)
-                # proc = subprocess.Popen( ['rename', tuple["rename_fmt"], fullpath], stdout=subprocess.PIPE )
-                # output = proc.communicate()
-
-                # fk this, fuck python. this is how you get it to work....
                 os.chdir(tuple["copy_dir"])
                 cmdstring = "rename " + tuple["rename_fmt"] + " " + "*.mkv"
                 print ( "%s" % cmdstring)
